c1bb0057b560ca45889830d3f8ff67e1f4a616a6
brotigen23 <brotigen23@gmail.com>
2023-10-22 23:15:23 +0300

    init

---
+++ Configs/.config/polybar/launch.sh
@@ -0,0 +1,5 @@
+#!/bin/bas
+
+killall -q polybar
+echo "---" | tee -a /tmp/polybar_example.log
+polybar example >>/tmp/polybar_example.logh
---
+++ setup.sh
@@ -0,0 +1,42 @@
+UpdateRepoDB(){
+	echo "Update pacman repo"
+	sudo pacman --noconfirm -Syu
+}
+
+InstallNeededPackages(){
+	echo "Install xorg"
+	sudo pacman --noconfirm -S xorg xorg-init
+	# sudo pacman --noconfirm -S xorg-server xorg-xinit xorg-xset xorg-xsetroot xorg-xrandr xorg-xrdb
+
+	echo "Install other packages"
+	sudo pacman --noconfirm -S bspwm sxhkd dmenu polybar picom xfce4-terminal gnu-free-fonts feh
+}
+
+InstallYay(){
+	echo "Install yay"
+	git clone https://aur.archlinux.org/yay.git 
+  cd ./yay || exit
+  makepkg -sir --noconfirm
+  cd ..
+  rm -rf yay
+}
+
+InstallBetterlockscreen(){
+	InstallYay
+	yay --noconfirm -S betterlockscreen
+}
+
+CopyConfigs(){
+	mkdir -p ~/.config
+	cp -rf ./Configs/.config/. ~/.config
+	cp -rf ./Config/xinitrc ~/
+	chmod +x ~/.config/bspwm/bspwmrc
+	chmod +x ~/.configpolybar/launch.sh
+}
+
+
+UpdateRepoDB
+InstallNeededPackages
+InstallYay
+
+CopyConfigs
